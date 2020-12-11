4381bf54532f36355a71beead1b640b9615d4fc8
Katiuska Alicea <katiuska@Katiuskas-MacBook-Air.local>
2020-12-10 22:26:58 -0800

    added some fields to the edit form hanve hancdle changes fn and part of the handle submit funct and addedl link to an edit button in the movie component and styled it usinf the save button styles

--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,10 +1,59 @@
-import React from "react"
+import React, { useState } from "react";
 
 const UpdateMovie = () => {
-    return(<div>
-        <h2>This is the Update form</h2>
-    </div>)
+  const [updateForm, setUpdateForm] = useState({
+    title: "",
+    director: "",
+    metascore: "",
+    stars: [],
+  });
 
-}
+  console.log("changes: ", updateForm);
 
-export default UpdateMovie
+  const handleChange = (e) => {
+    setUpdateForm({
+      ...updateForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUpdateForm({
+      title: "",
+      director: "",
+      metascore: "",
+    });
+  };
+  return (
+    <div>
+      <h2>This is the Update form</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter movie title"
+          name="title"
+          value={updateForm.title}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Enter Metascore"
+          name="metascore"
+          value={updateForm.metascore}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Enter director"
+          name="director"
+          value={updateForm.director}
+          onChange={handleChange}
+        />
+        <button>Save Changes</button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateMovie;
